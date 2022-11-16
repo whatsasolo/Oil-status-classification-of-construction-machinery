10d394a0a2c423edad821b033d1e341cb1f7d1fe
whatsasolo <dowsing.stealth-0d@icloud.com>
2022-11-16 23:34:30 +0900

    FEAT: ADD another DL stategy

---
+++ sub_train.py
@@ -0,0 +1,53 @@
+import math
+import time
+
+import numpy
+import pandas
+
+from torch.utils.data import Dataset, DataLoader
+
+def reset_weights(m):
+    '''
+        Try resetting model weights to avoid
+        weight leakage.
+    '''
+    for layer in m.children():
+        if hasattr(layer, 'reset_parameters'):
+            print(f'Reset trainable parameters of layer = {layer}')
+            layer.reset_parameters()
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (remain %s)' % (asMinutes(s), asMinutes(rs))
+
+
+class CategoryDataset(Dataset):
+    def __init__(self) -> None:
+        super().__init__()
+        
